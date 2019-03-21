c211bb9587a9733a5fed7ccc996f173cd8bd514d
mcorfmat <marc.corfmat@hotmail.com>
2019-03-21 16:20:23 -0700

    Completed EducationData object Class

--- src/EducationData.java
+++ src/EducationData.java
@@ -1,6 +1,61 @@
+import javax.swing.text.EditorKit;
+
 /***
  * Holds education data
  * @author Marc Corfmat
  */
 public class EducationData {
+    private String countyName;
+    private double percentNoHSDiploma;
+    private double percentHSDiplomaOnly;
+    private double percentCollegeOrAssociates;
+    private double percentBachelorOrHigher;
+
+    public EducationData(String countyName, double percentNoHSDiploma, double percentHSDiplomaOnly, double percentCollegeOrAssociates, double percentBachelorOrHigher) {
+        this.countyName = countyName;
+        this.percentNoHSDiploma = percentNoHSDiploma;
+        this.percentHSDiplomaOnly = percentHSDiplomaOnly;
+        this.percentCollegeOrAssociates = percentCollegeOrAssociates;
+        this.percentBachelorOrHigher = percentBachelorOrHigher;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public double getPercentNoHSDiploma() {
+        return percentNoHSDiploma;
+    }
+
+    public void setPercentNoHSDiploma(double percentNoHSDiploma) {
+        this.percentNoHSDiploma = percentNoHSDiploma;
+    }
+
+    public double getPercentHSDiplomaOnly() {
+        return percentHSDiplomaOnly;
+    }
+
+    public void setPercentHSDiplomaOnly(double percentHSDiplomaOnly) {
+        this.percentHSDiplomaOnly = percentHSDiplomaOnly;
+    }
+
+    public double getPercentCollegeOrAssociates() {
+        return percentCollegeOrAssociates;
+    }
+
+    public void setPercentCollegeOrAssociates(double percentCollegeOrAssociates) {
+        this.percentCollegeOrAssociates = percentCollegeOrAssociates;
+    }
+
+    public double getPercentBachelorOrHigher() {
+        return percentBachelorOrHigher;
+    }
+
+    public void setPercentBachelorOrHigher(double percentBachelorOrHigher) {
+        this.percentBachelorOrHigher = percentBachelorOrHigher;
+    }
 }
