1d3ce4f3a7b84be7eccbc45eadea273dd87f248c
Kevin Wang <kevinwang0324@gmail.com>
2019-03-21 23:26:52 -0700

    Made readFileAsString a general method

--- src/DataParser.java
+++ src/DataParser.java
@@ -1,6 +1,25 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /***
  * Parses all data
  * @author Kevin Wang
  */
 public class DataParser {
+    public static String readFileAsString(String filepath) {
+        StringBuilder output = new StringBuilder();
+
+        try (Scanner scanner = new Scanner(new File(filepath))) {
+
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                output.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
 }
