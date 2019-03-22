6771b6835e7c4abd2212c22481697d62eb2fb2c3
Kevin Wang <kevinwang0324@gmail.com>
2019-03-21 23:27:18 -0700

    Implemented House Data Parser

--- src/HouseData.java
+++ src/HouseData.java
@@ -3,4 +3,27 @@
  * @author Kevin Wang
  */
 public class HouseData {
+    private String countyName;
+    private int medianHousePrice;
+
+    public HouseData(String countyName, int medianHousePrice) {
+        this.countyName = countyName;
+        this.medianHousePrice = medianHousePrice;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getMedianHousePrice() {
+        return medianHousePrice;
+    }
+
+    public void setMedianHousePrice(int medianHousePrice) {
+        this.medianHousePrice = medianHousePrice;
+    }
 }
--- src/HouseParser.java
+++ src/HouseParser.java
@@ -2,5 +2,37 @@
  * Parses all house data
  * @author Kevin Wang
  */
+
+import java.util.ArrayList;
+
 public class HouseParser {
+
+    public static ArrayList<HouseData> parseHouseData(String houseFile) {
+        ArrayList<HouseData> results = new ArrayList<>();
+
+        String[] lines = houseFile.split("\n");
+
+        for (int i = 1; i < lines.length; i++) {
+            String line = lines[i];
+            line = cleanString(line);
+
+            String[] components = line.split(",");
+            results.add(new HouseData(components[0], Integer.parseInt(components[1])));
+        }
+
+        return results;
+    }
+
+    private static String cleanString(String line) {
+        int firstQuote = line.indexOf("\"");
+        int secondQuote = line.indexOf("\"", firstQuote + 1);
+
+        if (firstQuote != -1) {
+            String diff = line.substring(firstQuote, secondQuote + 1);
+            String copy = diff;
+            copy = copy.replaceAll("\"", "");
+            copy = copy.replaceAll(",", "");
+            line = line.replaceFirst(diff, copy);
+        }
+    }
 }
