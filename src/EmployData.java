0ef65ea972a2d81de04c66d66fa69e87ff839af6
ananyachilakamarthi <ananya.chilakamarthi@gmail.com>
2019-03-23 00:40:55 -0700

    Adding EmployData and EmployParser

--- src/EmployData.java
+++ src/EmployData.java
@@ -3,4 +3,32 @@
  * @author Ananya Chilakamarthi
  */
 public class EmployData {
+    private String county;
+    private float unemploymentRate;
+
+    public EmployData(String county, float unemploymentRate) {
+        this.county = county;
+        this.unemploymentRate = unemploymentRate;
+    }
+    public String getCounty(){
+        return county;
+    }
+
+    public void setCounty( String county){
+        this.county = county;
+    }
+
+    public float getUnemploymentRate(){
+        return unemploymentRate;
+    }
+     public void setUnemploymentRate(int unemploymentRate){
+        this.unemploymentRate = unemploymentRate;
+     }
+
+    public String toString() {
+        return (county + " " + unemploymentRate);
+    }
 }
+
+
+
--- src/EmployParser.java
+++ src/EmployParser.java
@@ -1,6 +1,52 @@
+import java.util.ArrayList;
+
 /***
  * Parses Employment Data
  * @author Ananya Chilakamarthi
  */
 public class EmployParser {
+    public static ArrayList<EmployData> parseData(String employFile) {
+        ArrayList<EmployData> finished = new ArrayList<>();
+
+        String[] lines = employFile.split("\n");
+
+        for (int i = 8; i < lines.length; i++) {
+            String eachLine = lines[i];
+            eachLine = clean(eachLine);
+            System.out.println(lines[i]);
+           // String[] tokenList = lines[i].split(",");
+            String[] tokenList = lines[i].split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+
+            float rate = 0;
+
+            System.out.println(tokenList.length);
+            if(tokenList[45] != ""){
+                rate = Float.parseFloat(tokenList[45]);
+            }
+
+            finished.add(new EmployData(tokenList[2], rate));
+
+        }
+
+        return finished;
+    }
+
+
+    private static String clean(String line) {
+
+            int q1 = line.indexOf("\"");
+            int q2 = line.indexOf("\"", q1 + 1);
+            if (q1 != -1) {
+                String orig = line.substring(q1, q2 + 1);
+                String replacement = orig;
+                replacement = replacement.replaceAll("\"", "");
+                replacement = replacement.replaceAll(",", "");
+                line = line.replace(orig, replacement);
+            }
+
+
+
+        return line;
+    }
 }
+
